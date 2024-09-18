62b31f271f50e416f038aadeb3b0b6c6c1660a2d
Roman Matsypula <r.matsypula@letyshops.com>
2024-09-19 00:53:54 +0300

    set tasks in asReadonly

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -4,7 +4,9 @@ import { Task } from './task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
+
+  allTasks = this.tasks.asReadonly();
 
   addTask(taskData: { title: string; description: string }) {
     const newTask: Task = {
